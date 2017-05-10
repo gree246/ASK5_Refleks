35c5ab4c8e19b9b182007cb55fccd5813002fbfa
gree246 <g.derda2@gmail.com>
2017-05-10 20:12:46 +0200

    gra1 b_start

--- ASK5_Refleks/ASK5_Refleks/gra1.Designer.cs
+++ ASK5_Refleks/ASK5_Refleks/gra1.Designer.cs
@@ -28,8 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.l_czas = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.l_opis = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.b_start = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,18 +50,44 @@
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Image = global::ASK5_Refleks.Properties.Resources.arrow;
-            this.pictureBox1.Location = new System.Drawing.Point(391, 176);
+            this.pictureBox1.Location = new System.Drawing.Point(392, 125);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(211, 105);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // l_opis
+            // 
+            this.l_opis.AutoSize = true;
+            this.l_opis.Location = new System.Drawing.Point(144, 387);
+            this.l_opis.Name = "l_opis";
+            this.l_opis.Size = new System.Drawing.Size(81, 17);
+            this.l_opis.TabIndex = 2;
+            this.l_opis.Text = "Opis todo...";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // b_start
+            // 
+            this.b_start.Location = new System.Drawing.Point(322, 271);
+            this.b_start.Name = "b_start";
+            this.b_start.Size = new System.Drawing.Size(326, 77);
+            this.b_start.TabIndex = 3;
+            this.b_start.Text = "START";
+            this.b_start.UseVisualStyleBackColor = true;
+            this.b_start.Click += new System.EventHandler(this.b_start_Click);
             // 
             // gra1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(970, 468);
+            this.Controls.Add(this.b_start);
+            this.Controls.Add(this.l_opis);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.l_czas);
             this.Name = "gra1";
@@ -72,5 +102,8 @@
 
         private System.Windows.Forms.Label l_czas;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label l_opis;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button b_start;
     }
 }
--- ASK5_Refleks/ASK5_Refleks/gra1.cs
+++ ASK5_Refleks/ASK5_Refleks/gra1.cs
@@ -12,9 +12,44 @@ namespace ASK5_Refleks
 {
     public partial class gra1 : Form
     {
+        Random random;
+        int ktora_strzalka;
+
+        int[] czas;
+        int l_prob = 0;
+
         public gra1()
         {
             InitializeComponent();
+            random =  new Random();
+            czas = new int[5];
+            
+            timer1.Enabled = false;
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            timer1.Enabled = false;
+            ktora_strzalka = random.Next() % 1;
+            switch (ktora_strzalka)
+            {
+                case 0:
+                    pictureBox1.Visible = true;
+                    break;
+            }
+
+            DateTime start = DateTime.Now;
+
+
+        }
+
+        private void b_start_Click(object sender, EventArgs e)
+        {
+            b_start.Visible = false;
+            timer1.Interval = (int)(2000*random.NextDouble()+1000);
+            timer1.Enabled = true;
         }
     }
 }
